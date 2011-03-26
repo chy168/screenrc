dcb55e4bf66af6b11b99c91a897667fd62b8da01
Zz Chen <chy168@gmail.com>
2011-03-26 16:33:28 +0000

    First version, screen, ssh helper

---
+++ ssh_list.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+TMP_FILE="/tmp/ssss"
+
+rm -rf ${TMP_FILE}
+
+declare -a LIST
+MENU_ITEMS=""
+count=0
+# LINE is hostname
+for LINE in `cat ssh_host.txt | awk '{print $2}'`; do
+	LIST[$count]=$LINE
+	MENU_ITEMS="${MENU_ITEMS} ${count} $LINE"
+	((count++))
+done
+
+echo $MENU_ITEMS
+#dialog --ascii-lines --menu "Zz SSH Connection Tool" 30 50 30 $(cat ssh_host.txt | tr '\n' ' ') 2> TMP_FILE
+dialog --ascii-lines --menu "Zz SSH Connection Tool" 30 50 30 ${MENU_ITEMS} 2> ${TMP_FILE}
+
+CHOOSE=`cat ${TMP_FILE}`
+
+#Set terminal name (work in screen)
+echo -e "\033k${LIST[$CHOOSE]}\033\\"
+ssh -l zz_chen ${LIST[$CHOOSE]}
+
+
